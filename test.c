0ae791f546b7fd0cd2819735e83efe9346a5da99
Jitwisut <138758990+Jitwisut@users.noreply.github.com>
2023-07-18 14:05:10 +0700

    Update test.c

--- test.c
+++ test.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
-int test(){
+/*int test(){
 	int a,b;
    for(a=1;a<=15;a++){
    
    	printf("%d \n",a);
    }
   
+} */
+void testa (){
+	int a,b,sum;
+	printf("Value of A : ");
+	scanf("%d",&a);
+	printf("Value of B : ");
+	scanf("%d",&b);
+	sum = a*b;
+	printf("Value of Sum : %d x %d = %d ",a,b,sum);
 }
+	
 int main(){
-	test();
+	testa();
 }
